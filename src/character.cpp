1220102118a85d1ddc5f439aff855b5b4288dcf4
Kolzza <yoshi88639@gmail.com>
2023-05-25 23:50:40 -0700

    setters and some other stuff in character.cpp

--- src/character.cpp
+++ src/character.cpp
@@ -20,20 +20,62 @@ Character::~Character() {
     int Character::getHappiness(){ return happiness; }
     int Character::getHealth() { return health; }
 
+
+    void Character::setGrades(int grades) {
+        this->grades = grades;
+    }
+    void Character::setHappiness(int happiness) {
+        this->happiness = happiness;
+    }
+    void Character::setHealth(int health) {
+        this->health = health;
+    }
+
     //stat modifiers (algorithm for increase subject to change, maybe increases and decreases)
     void Character::workout(){
         int increase = rand() % 10 + 1;
         health += increase;
 
+        if(increase <= 2){
+            cout << "You trip and fall over a loose dumbell. +" << increase; 
+        }
+        else if(increase > 2 or increase <= 4){
+            cout << "You feel lazy, so you just do cardio. +" << increase;
+        }
+        else if(increase > 4 or increase <= 6){
+            cout << "You have a good workout and feel sore the next day. +" << increase;
+        }
+        else if(increase > 6 or increase <= 8){
+            cout << "You have an amazing workout and get a new record on some of your sets. +" << increase;
+        }
+        else if(increase > 8 or increase <= 10){
+            cout << "You PR on everything you try and get some reputaton at the gym. +" << increase;
+        }
         //add 5-10 fun messages based on the value you get
-
+        cout << endl;   
 
     }
 
     void Character::studyHarder(){
         int increase = rand() % 10 + 1;
         grades += increase;
-
+        if(increase <= 2){
+            cout << "You get distracted by your phone. +" << increase; 
+        }
+        else if(increase > 2 or increase <= 4){
+            cout << "You study a little bit. +" << increase;
+        }
+        else if(increase > 4 or increase <= 6){
+            cout << "You join a study group and are ready for your test. +" << increase;
+        }
+        else if(increase > 6 or increase <= 8){
+            cout << "You study for a while. Maybe you can help teach class next time. +" << increase;
+        }
+        else if(increase > 8 or increase <= 10){
+            cout << "You master the subject. It is impossible to fail your exam. +" << increase;
+        }
+        //add 5-10 fun messages based on the value you get
+        cout << endl;   
         //add 5-10 fun messages based on the value you get
     }
     void Character::hangWithFriends(){
