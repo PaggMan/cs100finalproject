996bd7104de4db1e3b8e93f3c394c58e4820e87c
Kolzza <yoshi88639@gmail.com>
2023-06-08 22:20:02 -0700

    i am dumb again

--- tests/characterTests.cpp
+++ tests/characterTests.cpp
@@ -7,21 +7,21 @@ using namespace std;
 
 TEST(ModifierTests, workoutTest){
     Character* testSubject = new Character();
-    beforeWorkout = testSubject->getHealth();
+    int beforeWorkout = testSubject->getHealth();
     testSubject->workout();
     EXPECT_TRUE(beforeWorkout > testSubject->getHealth());
 }
 
 TEST(ModifierTests, studyTest){
     Character* testSubject = new Character();
-    beforeStudy = testSubject->getGrades();
+    int beforeStudy = testSubject->getGrades();
     testSubject->studyHarder();
     EXPECT_TRUE(beforeStudy > testSubject->getGrades());
 }
 
 TEST(ModifierTests, hangoutTest){
     Character* testSubject = new Character();
-    beforeHangout = testSubject->getHappiness();
+    int beforeHangout = testSubject->getHappiness();
     testSubject->hangWithFriends();
     EXPECT_TRUE(beforeHangout > testSubject->getHappiness());
 }
