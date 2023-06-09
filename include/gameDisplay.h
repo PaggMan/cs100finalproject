15daad1305a855182c56ac316c19556129f68396
PaggMan <pchen215@ucr.edu>
2023-06-09 14:23:26 -0700

    Delete useless files

--- include/gameDisplay.h
+++
@@ -1,9 +0,0 @@
-#ifndef GAMEDISPLAY_H
-#define GAMEDISPLAY_H
-#include <string>
-class GameDisplay {
-    private:
-        void outputMessage(const std::string& message);  //Uses cout to output string's contents to terminal
-};
-
-#endif
