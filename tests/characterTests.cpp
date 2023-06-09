1c185f828602d6219735e892b9322dec3430e08d
Kolzza <yoshi88639@gmail.com>
2023-06-08 21:37:23 -0700

    added character unit tests

---
+++ tests/characterTests.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+#include "../include/character.h"
+
+using namespace std;
+
+
+TEST(ModifierTests, workoutTest){
+    Character* testSubject = new Character()
+    ASSERT_EQ()
+}
